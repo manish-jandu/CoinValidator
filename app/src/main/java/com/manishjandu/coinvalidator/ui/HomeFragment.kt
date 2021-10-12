dfbd280d29743b29622a17e9ae618de4664a798c
Manish <manishjandu1@gmail.com>
2021-10-12 18:14:46 +0530

    home fragment and qrscanner fragment added

---
+++ app/src/main/java/com/manishjandu/coinvalidator/ui/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.manishjandu.coinvalidator.ui
+
+import androidx.fragment.app.Fragment
+import com.manishjandu.coinvalidator.R
+
+class HomeFragment:Fragment(R.layout.fragment_home) {
+}
---
+++ app/src/main/java/com/manishjandu/coinvalidator/ui/QrScannerFragment.kt
@@ -0,0 +1,8 @@
+package com.manishjandu.coinvalidator.ui
+
+import androidx.fragment.app.Fragment
+import com.manishjandu.coinvalidator.R
+
+
+class QrScannerFragment:Fragment(R.layout.fragment_qrscanner) {
+}
